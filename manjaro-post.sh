342a24c33395e624a826197b37faa3b14a75d8a2
vfalmado <61508951+vfalmado@users.noreply.github.com>
2022-08-15 19:24:21 -0300

    Addition of new programs

--- manjaro-post.sh
+++ manjaro-post.sh
@@ -14,20 +14,27 @@ sudo snap install authy multipass
 
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub org.gnome.Boxes -y
 flatpak install flathub com.elsevier.MendeleyDesktop -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.valvesoftware.Steam -y
+# flatpak install flathub io.github.shiftey.Desktop -y
 flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub org.kde.tellico -y
 flatpak install flathub org.kde.kdenlive -y
 flatpak install flathub org.kde.akregator -y
 flatpak install flathub com.gitlab.davem.ClamTk -y
+flatpak install flathub com.heroicgameslauncher.hgl -y
+flatpak install flathub com.eduke32.EDuke32 -y
+flatpak install flathub com.play0ad.zeroad -y
+flatpak install flathub com.todoist.Todoist -y
 sudo freshclam
 
-# Downloading PhotoGIMP
+# Downloading PhotoGIMP and Mendeley
 
 wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
+wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.75.0-x86_64.AppImage
 
 # Updating system keys
 
