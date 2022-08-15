9d9ca16935d7e2b5a9402c6e9893ca8f34d950b8
vfalmado <61508951+vfalmado@users.noreply.github.com>
2022-08-15 19:46:08 -0300

    Minor corrections

--- manjaro-post.sh
+++ manjaro-post.sh
@@ -12,28 +12,31 @@ sudo snap install authy multipass
 
 # Installing flatpak packages
 
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub net.cozic.joplin_desktop -y
-flatpak install flathub org.gnome.Boxes -y
-flatpak install flathub com.elsevier.MendeleyDesktop -y
-flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub com.valvesoftware.Steam -y
-# flatpak install flathub io.github.shiftey.Desktop -y
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub org.kde.tellico -y
-flatpak install flathub org.kde.kdenlive -y
-flatpak install flathub org.kde.akregator -y
-flatpak install flathub com.gitlab.davem.ClamTk -y
-flatpak install flathub com.heroicgameslauncher.hgl -y
-flatpak install flathub com.eduke32.EDuke32 -y
 flatpak install flathub com.play0ad.zeroad -y
+flatpak install flathub org.kde.akregator -y
+flatpak install flathub com.bitwarden.desktop -y
+flatpak install flathub com.gitlab.davem.ClamTk -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.eduke32.EDuke32 -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub org.gnome.Boxes -y
+# flatpak install flathub io.github.shiftey.Desktop -y
+flatpak install flathub com.heroicgameslauncher.hgl -y
+flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub org.kde.kdenlive -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.valvesoftware.Steam -y
+flatpak install flathub org.kde.tellico -y
 flatpak install flathub com.todoist.Todoist -y
+flatpak install flathub com.rtosta.zapzap -y
 sudo freshclam
 
 # Downloading PhotoGIMP and Mendeley
 
 wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
+mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.local ~
+mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.var ~
 wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.75.0-x86_64.AppImage
 
 # Updating system keys
@@ -44,7 +47,7 @@ wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.75.0-x
 
 # Installing Pamac packages
 
-pamac install telegram-desktop libreoffice-fresh lutris firefox-developer-edition gparted teamviewer
+pamac install firefox-developer-edition gparted libreoffice-fresh lutris teamviewer telegram-desktop
 
 # Updating system keys
 
