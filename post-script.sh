8b652039b62fb68371423be02cfed282982d0572
Vítor (Vitu) <61508951+vfalmado@users.noreply.github.com>
2023-11-13 18:08:56 -0300

    Update post-script.sh
    Removed unused packages, added new ones and updated links

--- post-script.sh
+++ post-script.sh
@@ -3,75 +3,54 @@ set -e
 
 # Setting variables
 
-REPS="btop exa ncdu duf bat tldr thefuck gparted ripgrep okular"
-FLATPAKS="com.play0ad.zeroad -y
-io.github.Foldex.AdwSteamGtk -y
-io.github.prateekmedia.appimagepool -y
-com.bitwarden.desktop -y
-com.usebottles.bottles -y
-com.gitlab.davem.ClamTk -y
-com.github.Eloston.UngoogledChromium -y
-org.gnome.DejaDup -y
-com.discordapp.Discord -y
-org.mozilla.firefox -y
-org.flameshot.Flameshot -y
-com.github.tchx84.Flatseal -y
-org.gimp.GIMP -y
-org.gnome.Extensions -y
-org.gnome.Boxes -y
-org.godotengine.Godot -y
-org.hedgewars.Hedgewars -y
-com.heroicgameslauncher.hgl -y
-net.cozic.joplin_desktop -y
-org.kde.kdenlive -y
-info.febvre.Komikku -y
-io.lmms.LMMS -y
-net.lutris.Lutris -y
-ch.theologeek.Manuskript -y
-nz.mega.MEGAsync -y
-org.musescore.MuseScore -y
-com.obsproject.Studio -y
-org.openttd.OpenTTD -y
-org.prismlauncher.PrismLauncher -y
-org.libretro.RetroArch -y
-net.scribus.Scribus -y
-dev.alextren.Spot -y
-com.spotify.Client -y
-com.valvesoftware.Steam -y
-flathub dev.storyapps.starc -y
-org.telegram.desktop -y
-org.kde.tellico -y
-org.mozilla.Thunderbird -y
-com.todoist.Todoist -y
-com.transmissionbt.Transmission -y
-org.tuxemon.Tuxemon -y
-com.rtosta.zapzap -y"
-SNAPS="authy multipass"
-
+REPS="btop exa ncdu duf bat tldr thefuck gparted ripgrep"
+FLATPAKS="io.github.Foldex.AdwSteamGtk
+com.bitwarden.desktop
+com.usebottles.bottles
+org.gnome.Boxes
+com.gitlab.davem.ClamTk
+org.gnome.DejaDup
+com.discordapp.Discord
+org.mozilla.firefox
+com.github.tchx84.Flatseal
+org.gimp.GIMP
+org.gnome.Extensions
+com.heroicgameslauncher.hgl
+org.kde.kdenlive
+info.febvre.Komikku
+io.lmms.LMMS
+net.lutris.Lutris
+nz.mega.MEGAsync
+com.obsproject.Studio
+org.kde.okular
+org.libretro.RetroArch
+com.spotify.Client
+com.valvesoftware.Steam
+org.telegram.desktop
+org.kde.tellico
+org.mozilla.Thunderbird
+com.todoist.Todoist
+com.transmissionbt.Transmission
+org.tuxemon.Tuxemon
+com.rtosta.zapzap"
 
 # Identifying the package manager, updating, and enabling snaps
 
 # APT
 if command -v apt > /dev/null 2>&1; then
   sudo apt update && sudo apt upgrade -y
-  sudo rm /etc/apt/preferences.d/nosnap.pref
   sudo apt update
-  sudo apt install snapd -y
 
 # DNF
 elif command -v dnf > /dev/null 2>&1; then
   sudo dnf update -y
-  sudo dnf install snapd akmod-nvidia -y
+  sudo dnf install akmod-nvidia -y
   
 # PACMAN
 elif command -v pacman > /dev/null 2>&1; then
   sudo pacman -Syu
-  sudo pacman -S flatpak snapd
+  sudo pacman -S flatpak
 fi
-
-# Enabling Snaps
-
-sudo systemctl enable --now snapd.socket
 
 # Adding flathub, installing flatpak packages, and updating ClamTK and flatpaks
 
@@ -80,10 +59,6 @@ flatpak install flathub $FLATPAKS
 flatpak update -y
 sudo freshclam
 
-# Installing Snaps
-
-sudo snap install $SNAPS
-
 # Downloading PhotoGIMP, Syncthing, Bedrock Linux, Mendeley and NordVPN
 
 curl -s https://raw.githubusercontent.com/projetus-ti/facilitador-linux/master/install.sh | sudo bash
@@ -91,13 +66,13 @@ wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Di
 unzip PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
 mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.local ~
 mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.var ~
-wget https://github.com/syncthing/syncthing/releases/download/v1.23.1/syncthing-linux-amd64-v1.23.1.tar.gz
-tar -xzf syncthing-linux-amd64-v1.23.1.tar.gz
+wget https://github.com/syncthing/syncthing/releases/download/v1.26.0/syncthing-linux-amd64-v1.26.0.tar.gz
+tar -xzf syncthing-linux-amd64-v1.26.0.tar.gz
 wget https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.28/bedrock-linux-0.7.28-x86_64.sh
 chmod +x bedrock-linux-0.7.28-x86_64.sh
 sudo sh bedrock-linux-0.7.28-x86_64.sh --hijack
-wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.75.0-x86_64.AppImage
-rm PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip syncthing-linux-amd64-v1.23.1.tar.gz bedrock-linux-0.7.28-x86_64.sh
+wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.103.0-x86_64.AppImage
+rm PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip syncthing-linux-amd64-v1.26.0.tar.gz bedrock-linux-0.7.28-x86_64.sh
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 nordvpn login
 
@@ -113,9 +88,9 @@ if command -v pacman > /dev/null 2>&1; then
 # Installing TeamViewer, dnf packages and system updates 
 
 elif command -v dnf > /dev/null 2>&1; then
-  wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm https://mega.nz/linux/repo/Fedora_37/x86_64/megasync-Fedora_37.x86_64.rpm
-  sudo dnf install teamviewer.x86_64.rpm megasync-Fedora_37.x86_64.rpm
-  rm teamviewer.x86_64.rpm megasync-Fedora_37.x86_64.rpm
+  wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm #https://mega.nz/linux/repo/Fedora_37/x86_64/megasync-Fedora_37.x86_64.rpm
+  sudo dnf install teamviewer.x86_64.rpm #megasync-Fedora_37.x86_64.rpm
+  rm teamviewer.x86_64.rpm #megasync-Fedora_37.x86_64.rpm
   sudo dnf install fd-find gnome-tweaks $REPS
   sudo dnf update
   
