4efe517cb7246807c1b5a31e877257e2030beb81
Vítor (Vitu) <61508951+vfalmado@users.noreply.github.com>
2024-10-29 17:43:37 -0300

    Update post-script.sh
    software changes and updates

--- post-script.sh
+++ post-script.sh
@@ -3,39 +3,35 @@ set -e
 
 # Setting variables
 
-REPS="adb bat btop duf exa gnome-tweaks gparted ncdu ripgrep timeshift tldr thefuck"
+REPS="adb bat btop duf exa gparted ncdu ripgrep timeshift tldr thefuck"
 FLATPAKS="io.github.Foldex.AdwSteamGtk
-flathub com.anydesk.Anydesk
-flathub org.ardour.Ardour
+net.ankiweb.Anki
+com.anydesk.Anydesk
+org.ardour.Ardour
 org.audacityteam.Audacity
 com.bitwarden.desktop
-com.rafaelmardojai.Blanket
 org.bleachbit.BleachBit
 com.usebottles.bottles
 org.gnome.Boxes
-hu.kramo.Cartridges
+studio.kx.carla
 com.gitlab.davem.ClamTk
 org.cockpit_project.CockpitClient
 org.darktable.Darktable
 com.neatdecisions.Detwinner
 com.discordapp.Discord
 org.mozilla.firefox
+org.flameshot.Flameshot
 com.github.tchx84.Flatseal
-im.fluffychat.Fluffychat
-org.freeplane.App
 io.github.cboxdoerfer.FSearch
 it.mijorus.gearlever
 org.gimp.GIMP
-org.gnome.Extensions
 org.guitarix.Guitarix
 com.heroicgameslauncher.hgl
-io.github.jeffshee.Hidamari
 org.inkscape.Inkscape
 org.kde.kdenlive
 info.febvre.Komikku
-io.lmms.LMMS
 net.lutris.Lutris
-nz.mega.MEGAsync
+net.minetest.Minetest
 net.agalwood.Motrix
 com.obsproject.Studio
 org.kde.okular
@@ -51,19 +47,18 @@ org.kde.tellico
 org.mozilla.Thunderbird
 com.todoist.Todoist
 com.transmissionbt.Transmission
-org.tuxemon.Tuxemon
 ar.com.tuxguitar.TuxGuitar
 org.videolan.VLC
 com.rtosta.zapzap"
 
-# Identifying the package manager, updating, and enabling snaps
+# Identifying the package manager and updating
 
 # APT
 if command -v apt > /dev/null 2>&1; then
   sudo apt update && sudo apt upgrade -y
   sudo apt update
 
-# DNF
+# DNF, also installs Nvidia drivers
 elif command -v dnf > /dev/null 2>&1; then
   sudo dnf update -y
   sudo dnf install akmod-nvidia -y
@@ -81,23 +76,18 @@ flatpak install flathub $FLATPAKS
 flatpak update -y
 sudo freshclam
 
-# Downloading PhotoGIMP, Fedistar, Syncthing, Universal Android Debloater, Bedrock Linux, Mendeley and NordVPN
+# Downloading PhotoGIMP, Universal Android Debloater, Bedrock Linux and NordVPN
 
-curl -s https://raw.githubusercontent.com/projetus-ti/facilitador-linux/master/install.sh | sudo bash
-wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
-unzip PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip
-mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.local ~
-mv PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak/.var ~
-wget https://github.com/h3poteto/fedistar/releases/download/v1.7.3/fedistar_1.7.3_amd64.AppImage
-wget https://github.com/syncthing/syncthing/releases/download/v1.26.0/syncthing-linux-amd64-v1.26.0.tar.gz
-tar -xzf syncthing-linux-amd64-v1.26.0.tar.gz
+wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.zip
+unzip PhotoGIMP.zip
+mv PhotoGIMP-master/.local ~
+mv PhotoGIMP-master/.var ~
 wget https://github.com/0x192/universal-android-debloater/releases/download/0.5.1/uad_gui-linux.tar.gz
 tar -xzf uad_gui-linux.tar.gz
-wget https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.28/bedrock-linux-0.7.28-x86_64.sh
-chmod +x bedrock-linux-0.7.28-x86_64.sh
-sudo sh bedrock-linux-0.7.28-x86_64.sh --hijack
-wget https://static.mendeley.com/bin/desktop/mendeley-reference-manager-2.103.0-x86_64.AppImage
-rm PhotoGIMP.by.Diolinux.v2020.1.for.Flatpak.zip syncthing-linux-amd64-v1.26.0.tar.gz bedrock-linux-0.7.28-x86_64.sh uad_gui-linux.tar.gz
+wget https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.30/bedrock-linux-0.7.30-x86_64.sh
+chmod +x bedrock-linux-0.7.30-x86_64.sh
+sudo sh bedrock-linux-0.7.30-x86_64.sh --hijack
+rm PhotoGIMP-master bedrock-linux-0.7.30-x86_64.sh uad_gui-linux.tar.gz
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 nordvpn login
 
@@ -110,13 +100,13 @@ if command -v pacman > /dev/null 2>&1; then
   pamac install libreoffice-fresh teamviewer fd $REPS
   sudo pacman -Syu
 
-# Installing TeamViewer, dnf packages and system updates 
+# Installing TeamViewer, MegaSync, dnf packages and system updates 
 
 elif command -v dnf > /dev/null 2>&1; then
-  wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm #https://mega.nz/linux/repo/Fedora_37/x86_64/megasync-Fedora_37.x86_64.rpm
-  sudo dnf install teamviewer.x86_64.rpm #megasync-Fedora_37.x86_64.rpm
-  rm teamviewer.x86_64.rpm #megasync-Fedora_37.x86_64.rpm
-  sudo dnf install fd-find gnome-tweaks $REPS
+  wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm https://mega.nz/linux/repo/Fedora_40/x86_64/megasync-Fedora_40.x86_64.rpm
+  sudo dnf install teamviewer.x86_64.rpm megasync-Fedora_40.x86_64.rpm
+  rm teamviewer.x86_64.rpm megasync-Fedora_40.x86_64.rpm
+  sudo dnf install fd-find glibc-devel cairo-devel libX11-devel xorg-x11-proto-devel lv2-devel $REPS
   sudo dnf update
   
 # Installing TeamViewer, apt packages and system updates
